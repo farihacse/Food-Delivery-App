b889747772a7480706fe95b59578594a995911a8
Fariha Noor <fariha.git@gmail.com>
2021-01-23 23:17:36 +0300

    finished UI design

--- app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
@@ -3,13 +3,24 @@ package com.example.fooddeliveryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test comment
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+
+
+
     }
 }
